6f13a248e119fa98fe79fa4475aed39b354a22d2
Shaw511 <3139590980@qq.com>
2024-01-27 17:53:36 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module MagiQuestopia
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net"
+	"net/http"
+)
+
+type User struct {
+	Name  string
+	Email string
+}
+
+func printIP() {
+	// 局域网IP获取
+	fmt.Println("局域网IP获取如下：")
+	// 获取所有网络接口的信息
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("Failed to get network interfaces:", err)
+		return
+	}
+
+	// 遍历网络接口
+	for _, iface := range interfaces {
+		// 排除回环接口和无效接口
+		if iface.Flags&net.FlagLoopback == 0 && iface.Flags&net.FlagUp != 0 {
+			// 获取接口的IP地址
+			addrs, err := iface.Addrs()
+			if err != nil {
+				fmt.Println("Failed to get addresses:", err)
+				continue
+			}
+
+			// 遍历IP地址
+			for _, addr := range addrs {
+				// 检查IP地址是否为IPNet类型，且为IPv4或IPv6地址
+				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						fmt.Println("IPv4 Address:", ipnet.IP)
+					} else if ipnet.IP.To16() != nil {
+						fmt.Println("IPv6 Address:", ipnet.IP)
+					}
+				}
+			}
+		}
+	}
+	//公网IP获取
+	fmt.Println("公网IP获取如下:")
+	// 向一个提供公网IP查询服务的网站发送GET请求
+	resp, err := http.Get("https://api.ipify.org?format=text")
+	if err != nil {
+		fmt.Println("Failed to get public IP:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// 读取响应的内容
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to read response:", err)
+		return
+	}
+
+	fmt.Println("Public IP:", string(ip))
+}
+
+func myWeb(wr http.ResponseWriter, req *http.Request) {
+	//fmt.Fprintf(wr, "服务器开启")
+	// 解析模板文件
+	templates, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		fmt.Println("Failed to parse template:", err)
+		return
+	}
+	// 创建一个数据对象
+	user := User{
+		Name:  "John Doe",
+		Email: "johndoe@example.com",
+	}
+
+	// 执行模板，并将生成的内容写入//
+	err = templates.ExecuteTemplate(wr, "index.html", user)
+	if err != nil {
+		fmt.Println("Failed to execute template:", err)
+		return
+	}
+
+}
+
+func main() {
+	printIP()
+	http.HandleFunc("/", myWeb)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		fmt.Println("Err in Server：", err)
+	}
+
+}
