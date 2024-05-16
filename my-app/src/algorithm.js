eaa98ace0fb9bc6435a61710fad4ac665aed9389
Shaw511 <3139590980@qq.com>
2024-05-17 00:35:55 +0800

    前端修改

--- my-app/src/algorithm.js
+++ my-app/src/algorithm.js
@@ -10,8 +10,23 @@ function Algorithm () {
     const [loading, setLoading] = useState(false);
     const [result, setResult] = useState('');
     const [imageData, setImageData] = useState('');
+    const [tooltip, setTooltip] = useState('');
+    const [isChecked1, setIsChecked1] = useState(false);
+    const [isChecked2, setIsChecked2] = useState(false);
 
-    const handleButtonClick = async (mode, command, datasetName, numPerturbSamples, topNode) => {
+    const handleCheckbox1Change = () => {
+        setIsChecked1(!isChecked1);
+    };
+
+    const handleCheckbox2Change = () => {
+        setIsChecked2(!isChecked2);
+    };
+
+    const handleButtonClick = async (action, mode, command, datasetName, numPerturbSamples, topNode) => {
+        if (!datasetName || !numPerturbSamples || !topNode) {
+            alert("请先在输入框中键入参数！");
+            return;
+        }
         setLoading(true);
         try {
             //建立WebSocket连接
@@ -26,14 +41,6 @@ function Algorithm () {
                 setResult(event.data);
                 setLoading(false);
             };
-            // socket.onopen = () => {
-            //     console.log('WebSocket Client Connected');
-            //     socket.send('subscribe_algorithm_result_queue');
-            // };
-            // socket.onmessage = (message) => {
-            //     const data = JSON.parse(message.data);
-            //     setImageData(data.imageData);
-            // };
 
             socket.onerror = function(event)  {
                 const error = event.error;
@@ -73,22 +80,15 @@ function Algorithm () {
                 />
             </div>
             <div className="button-container">
-                {/*<button*/}
-                {/*    onClick={() => {*/}
-                {/*        const isWindows = navigator.platform.includes('Win');*/}
-                {/*        const command = isWindows ? 'powershell -Command "Invoke-WebRequest -Uri https://github.com/Shaw511/PGMExplainer/tree/master/PGM_Node -OutFile ..\\Algorithms\\Data\\PGM_Node"' : 'wget https://github.com/Shaw511/PGMExplainer/tree/master/PGM_Node -P ./Algorithms/Data';*/}
-                {/*        handleButtonClick(command, datasetName, numPerturbSamples, topNode);*/}
-                {/*    }}*/}
-                {/*    disabled={loading}*/}
-                {/*>*/}
-                {/*Load Github Code*/}
-                {/*</button>*/}
                 <button
+                    onMouseEnter={() => setTooltip('gen_data')}
+                    onMouseLeave={() => setTooltip('')}
                     onClick={() => handleButtonClick('gen_data', `python3 GenData.py --dataset ${datasetName}`, datasetName, numPerturbSamples, topNode)}
                     disabled={loading}
                 >
-                    Generate Data 合成数据集
+                    合成数据集
                 </button>
+                <div className={`tooltip ${tooltip === 'gen_data' ? 'show' : ''}`}>在基图的基础上进行添加或减少随机边的扰动操作，添加特征结构motif，并对每个节点添加特征值。</div>
                 {/*<button*/}
                 {/*    onClick={() => handleButtonClick('gen_truth', `python3 GenGroundTruth.py --dataset ${datasetName}`, datasetName, numPerturbSamples, topNode)}*/}
                 {/*    disabled={loading}*/}
@@ -96,31 +96,54 @@ function Algorithm () {
                 {/*    Generate Ground Truth*/}
                 {/*</button>*/}
                 <button
+                    onMouseEnter={() => setTooltip('train_model')}
+                    onMouseLeave={() => setTooltip('')}
                     onClick={() => handleButtonClick('train_model', `python3 train.py --dataset ${datasetName}`, datasetName, numPerturbSamples, topNode)}
                     disabled={loading}
                 >
-                    Train Model 训练模型
+                    训练模型
                 </button>
+                <div className={`tooltip ${tooltip === 'train_model' ? 'show' : ''}`}>采用三层的图神经网络，将特征个数作为输入维度和层数，经过前向传播过程，返回[batch_size x num_nodes x embedding]格式的三维嵌入矩阵，将数据集划分为训练集、检验集和测试集，在1000个epoch上迭代训练，使用0.001的学习率，优化器采用Adam优化器。</div>
                 <button
+                    onMouseEnter={() => setTooltip('explain_pgm')}
+                    onMouseLeave={() => setTooltip('')}
                     onClick={() => handleButtonClick('explain_pgm', `python3 main.py --dataset ${datasetName} --num-perturb-samples ${numPerturbSamples} --top-node ${topNode}`, datasetName, numPerturbSamples, topNode)}
                     disabled={loading}
                 >
-                    PGM Explain 生成解释
+                    生成解释
                 </button>
+                <div className={`tooltip ${tooltip === 'explain_pgm' ? 'show' : ''}`}>对生成和预处理之后的样本数据进行扰动，形成给定p值的伯努利分布，观察扰动数据后的预测结果是否发生变化，如果变化则将待解释节点的目标样本值设为1，否则设为0。在对数据格式处理后，对于目标节点的邻居节点，分别计算卡方检验值和因果效应值，其中因果效应值代表了邻居节点对预测结果影响的显著性水平。</div>
                 <button
+                    onMouseEnter={() => setTooltip('eval_explain')}
+                    onMouseLeave={() => setTooltip('')}
                     onClick={() => handleButtonClick('eval_explain', `python3 evaluate_explanations.py --dataset ${datasetName} --num-perturb-samples ${numPerturbSamples} --top-node ${topNode}`, datasetName, numPerturbSamples, topNode)}
                     disabled={loading}
                 >
-                    Evaluate Explanations 指标评估
+                    指标评估
                 </button>
+                <div className={`tooltip ${tooltip === 'eval_explain' ? 'show' : ''}`}>每个数据集上的Accuracy和Precision</div>
                 {loading ? <p3>Loading...</p3> : <p3>{result}</p3>}
             </div>
+
             <div className="image-container">
                 {imageData && <img src={`data:image/png;base64,${imageData}`} alt="Graph"/>}
             </div>
+
+            <div className="checkbox-container">
+                <div>
+                    <input type="checkbox" checked={isChecked1} onChange={handleCheckbox1Change}/>
+                    <label>确认服务端已启动</label>
+                </div>
+                <div>
+                    <input type="checkbox" checked={isChecked2} onChange={handleCheckbox2Change}/>
+                    <label>确认参数已填写</label>
+                </div>
+            </div>
+
+
         </div>
     );
 
-};
+    };
 
-export default Algorithm;
+    export default Algorithm;
